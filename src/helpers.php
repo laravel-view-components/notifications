c4e5555e6b68248a32242b6f6a23f13cb567f3ee
Dan Wiser <danswiser@users.noreply.github.com>
2020-05-21 23:43:19 -0400

    wip

--- src/helpers.php
+++ src/helpers.php
@@ -7,9 +7,9 @@
  * @param string|array $class
  * @return null
  */
-if (! function_exists('flash')) {
-  function flash(string $text = null, $class = null)
-  {
-    return null;
-  }
-}
+// if (! function_exists('flash')) {
+//   function flash(string $text = null, $class = null)
+//   {
+//     return null;
+//   }
+// }
