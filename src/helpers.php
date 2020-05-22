6322be114348749bfc02d0251e6359483f5ebff8
Dan Wiser <danswiser@users.noreply.github.com>
2020-05-21 22:53:07 -0400

    wip

---
+++ src/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Helper to flash an alert to the screen.
+ *
+ * @param string $text
+ * @param string|array $class
+ * @return null
+ */
+if (! function_exists('flash')) {
+  function flash(string $text = null, $class = null)
+  {
+    return null;
+  }
+}
